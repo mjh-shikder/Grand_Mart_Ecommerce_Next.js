3b8af05378b11098adafcc389ecfd99c5e1c061e
mjh-shikder <mjhsikder@gmail.com>
2026-02-07 13:15:55 +0600

    Font installed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Lexend } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const lexend = Lexend({
+  weight:["100","200","300","400","500","600","800"],
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lexend.className} antialiased`}
       >
         {children}
       </body>
