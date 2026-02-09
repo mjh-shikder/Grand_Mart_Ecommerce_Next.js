0062ad20605b413d51110c1aafc3a2a3290b3781
mjh-shikder <mjhsikder@gmail.com>
2026-02-09 12:39:02 +0600

    Navbar added

---
+++ src/components/layouts/Logo.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <Link href={"/"}>
+            <Image alt='logo-grand-mart' src={'/assets/logo.png'} width={90} height={60} />
+        </Link>
+    );
+};
+
+export default Logo;
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import NavLink from "../buttons/NavLink";
+import { MdShoppingCart } from "react-icons/md";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  const links = (
+    <>
+      <li>
+        <NavLink href={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink href={"/products"}>Products</NavLink>
+      </li>
+      <li>
+        <NavLink href={"/blog"}>Blog</NavLink>
+      </li>
+      <li>
+        <NavLink href={"/contact"}>Contact</NavLink>
+      </li>
+     
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
         </div>
-    );
+        <Logo></Logo>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+          <div className="navbar-end md:space-x-3.5">
+              <Link href={"/cart"} className="btn btn-primary rounded-xl  " >  <MdShoppingCart size={30}></MdShoppingCart> </Link>
+              <Link href={"/login"}>
+                <button className="btn btn-secondary btn-outline rounded-xl ">Login</button>
+              </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
