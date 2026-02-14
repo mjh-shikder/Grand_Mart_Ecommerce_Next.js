4e05270ca7ad61b616bf71f156533154504438b9
mjh-shikder <mjhsikder@gmail.com>
2026-02-14 11:43:37 +0600

    Not found page added

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import Card from '@/components/ErrorCard';
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <div className='flex flex-col min-h-screen justify-center items-center'>
+            <Card></Card>
+        </div>
+    );
+};
+
+export default Error404;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,6 +1,12 @@
 import Products from '@/components/home/Products';
 import React from 'react';
 
+
+export const metadata = {
+    title: "All Products",
+    description: "Grand Mart"
+}
+
 const ProductsPage = () => {
     return (
         <div>
